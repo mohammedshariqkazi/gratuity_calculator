d135157e3cadba235ff92c5766e6da9ca62667d1
mohammedshariqkazi <shariqkazi201@gmail.com>
2023-07-18 14:39:41 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
       const years = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365));
       const days = Math.floor((differenceInMilliseconds % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
       const daysPerYear = years >= 5 ? 30 : 21;
-      const gratuity = (basicSalary * years * daysPerYear) / 365;
+      const gratuity = (basicSalary * years * daysPerYear) / 30;
 
       setGratuityAmount(gratuity);
       setYearsServed(years);
@@ -52,7 +52,36 @@ const App = () => {
     <div className="container">
       <img src={logo} alt="Gratuity Calculator Logo" className="logo" onClick={handleLogoClick} />
       <h1>Gratuity Calculator</h1>
-      {/* ... (Existing code) */}
+      <div>
+        <label htmlFor="joining-date">Date of Joining:</label>
+        <input type="date" id="joining-date" value={joiningDate} onChange={(e) => setJoiningDate(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="current-date">End of Service Date:</label>
+        <input type="date" id="current-date" value={currentDate} onChange={(e) => setCurrentDate(e.target.value)} />
+      </div>
+      <div className="input-container">
+        <label htmlFor="basic-salary">Basic Salary (in selected currency):</label>
+        <input
+          type="number"
+          id="basic-salary"
+          min="0"
+          step="0.01"
+          placeholder="Enter your basic salary"
+          value={basicSalary}
+          onChange={(e) => setBasicSalary(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="currency">Select Currency:</label>
+        <select id="currency" value={currency} onChange={(e) => setCurrency(e.target.value)}>
+          <option value="QAR">Qatari Riyal (QAR)</option>
+          <option value="USD">US Dollar (USD)</option>
+          <option value="EUR">Euro (EUR)</option>
+          <option value="GBP">British Pound (GBP)</option>
+          {/* Add more currency options as needed */}
+        </select>
+      </div>
       <button onClick={calculateGratuity}>Calculate Gratuity</button>
       {isLoading ? (
         <div className="loading">
